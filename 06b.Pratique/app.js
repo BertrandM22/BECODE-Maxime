f11df3d32976a0fd55c5315b131dd04e15fa2099
MaximeB <maxounu@gmail.com>
2024-09-05 13:40:30 +0200

    Exercices 06b pratique est terminer.

---
+++ 06b.Pratique/app.js
@@ -0,0 +1,25 @@
+
+// Premiere partie !
+
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE: 
+let whisper = message.trim().toLowerCase(); console.log(whisper)
+
+// Seconde partie !
+
+const name= "Dumbledore".slice(-1) 
+console.log(name)
+
+// Troisieme partie !
+// J'ai mis plusieurs facon.
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+let facialhairt= word.slice(+5)
+console.log(facialhairt)
+console.log( facialhairt.replace("board", "beard"))
+let facialhair = word.replace("board", "beard")
+console.log( facialhair)
+// YOUR CODE BELOW THIS LINE:
