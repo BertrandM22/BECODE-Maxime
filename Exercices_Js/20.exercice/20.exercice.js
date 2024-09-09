5f7f11136fe3bf16d8397ef7df3d1dc5a77a2ebb
MaximeB <maxounu@gmail.com>
2024-09-09 12:52:12 +0200

    ok

---
+++ Exercices_Js/20.exercice/20.exercice.js
@@ -0,0 +1,13 @@
+
+
+const tache= prompt('choisi une tache "new" "list" "delete" ou "quit" ')
+list=[]
+
+if (tache == "new") { 
+      list[list]=prompt("ajouter une action a la list"); 
+    
+  console.log(list)  
+} 
+if (tache == "list") { console.log(list)
+    
+}
