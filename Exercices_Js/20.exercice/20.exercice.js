50283e1d6e0271137496b93db28dd800b4fc66c9
MaximeB <maxounu@gmail.com>
2024-09-10 12:21:30 +0200

    fini

--- Exercices_Js/20.exercice/20.exercice.js
+++ Exercices_Js/20.exercice/20.exercice.js
@@ -1,13 +1,38 @@
+// const tache= prompt('choisi une tache "new" "list" "delete" ou "quit" ')
+list = ["a", "ad"];
+let quit = "";
 
+for (i = 0; i < list.length; i++) {
+  while (quit === "") {
+    tache = prompt('choisi une tache "new" "list" "delete" ou "quit" ');
 
-const tache= prompt('choisi une tache "new" "list" "delete" ou "quit" ')
-list=[]
+    if (tache == "new") {
+      let add = prompt("ajouter une action a la list");
+      list.push(add);
 
-if (tache == "new") { 
-      list[list]=prompt("ajouter une action a la list"); 
-    
-  console.log(list)  
-} 
-if (tache == "list") { console.log(list)
-    
+      list.forEach((element, index ) => {
+        console.log(index +" "+ element)
+      });
+    }
+
+    else if (tache == "list") {
+        list.forEach((element, index ) => {
+            console.log(index +" "+ element)
+          })
+    } 
+    else if (tache == "delete") {
+      
+        
+      let del = prompt("donne l'index a supprimer");
+      list.splice(del - 0, 1);
+
+      list.forEach((element, index ) => {
+        console.log(index +" "+ element)
+      })
+        
+    } 
+    else if (tache == "quit") {
+      quit = true;
+    }
+  }
 }
